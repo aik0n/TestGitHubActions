9e9940d524ed200b767e61ed904fb1fc355aa946
Aleksandr Kharlamov <alex-kharlamov@yandex.ru>
2025-11-17 15:08:26 +0300

    A stub template project and tests added

---
+++ Tests/WebApp.Tests.Unit/UnitTest1.cs
@@ -0,0 +1,13 @@
+namespace WebApp.Tests.Unit
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            var a = 5;
+
+            Assert.Equal(5, a);
+        }
+    }
+}
